91a406f5520847407baa58beac31aefe1084a3cd
agwam <agwamayatullah@gmail.com>
2024-07-07 21:01:59 +0800

    embed powerBI iFrame in dashboard page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,16 @@
 function Dashboard() {
-  return <div>{/* Your component content here */}</div>;
+  return (
+    <div className='home-content py-3 h-100'>
+      <iframe
+        title='Inwarung Dashboard'
+        width='1140'
+        height='541.25'
+        src='https://app.powerbi.com/reportEmbed?reportId=4c0dab4a-68b5-41ba-9efc-8c0266af231e&autoAuth=true&ctid=fea6da89-dfaf-49e3-ad81-5625aac89ab4'
+        frameBorder='0'
+        allowFullScreen='true'
+      ></iframe>
+    </div>
+  );
 }
 
 export default Dashboard;
